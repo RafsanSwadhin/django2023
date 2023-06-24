c3d644cadfce7163a83013207b2594bb5d59d314
RafsanSwadhin <126243472+RafsanSwadhin@users.noreply.github.com>
2023-06-24 14:42:50 +0600

    Add files via upload

---
+++ day_4/firstproject/firstproject/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import HttpResponse, render
+from tution.models import Contact
+
+def home (request):
+    name_list1 = ['Rafsan','Ahmed','Swadhin']
+    name_list2 = ['Feroz','Akash','Sakib']
+    name_list3 = ['Pappu','Fahim','Emon']
+    context = {
+        'name_1' : name_list1,
+        'name_2' : name_list2,
+        'name_3' : name_list3,
+    }
+    #return HttpResponse("Hello World")
+    return render(request, 'home.html',context)
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST['name']
+        phone = request.POST['phone']
+        content = request.POST['content']
+        obj = Contact(name = name, phone = phone, content = content)
+        obj.save()
+      
+    return render(request,'contact.html')
